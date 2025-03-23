30030fb448696305b151a4a31c3406b217c18f03
Ion Moldoveanu <imoldovea@yahoo.co.uk>
2025-03-23 19:00:31 +0200

    update log filter

--- utils.py
+++ utils.py
@@ -57,7 +57,8 @@ def setup_logging():
     # Create formatter
     formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
     # Add more substrings as needed
-    forbidden_logs = ["findfont", "werkzeug","werkzeug:_internal.py","dash-update-component","internal.py","pydevd"]
+    forbidden_logs = ["findfont", "werkzeug", "werkzeug:_internal.py", "dash-update-component", "internal.py", "pydevd",
+                      "wandb:"]
     # Console handler for INFO level and above
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
